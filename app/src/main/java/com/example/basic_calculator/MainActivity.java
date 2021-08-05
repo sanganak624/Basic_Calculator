1a0882cdde66743d5d404aecda6bbc4aed1401d5
Sang <sanganak624@gmail.com>
2021-08-06 01:15:45 +0800

    Finished worksheet

--- app/src/main/java/com/example/basic_calculator/MainActivity.java
+++ app/src/main/java/com/example/basic_calculator/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText varOneBox;
@@ -15,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
     private Button divideButton;
     private Button multiplyButton;
     private TextView resultBox;
+    private double var1=0;
+    private double var2=0;
+    private double result;
+    private boolean fail1=false;
+    private boolean fail2=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +31,118 @@ public class MainActivity extends AppCompatActivity {
         varTwoBox = (EditText) findViewById(R.id.varTwo);
 
         plusButton = (Button) findViewById(R.id.plus_Button);
+        minusButton = (Button) findViewById(R.id.minus_Button);
+        divideButton = (Button) findViewById(R.id.divide_Button);
+        multiplyButton = (Button) findViewById(R.id.multiply_Button);
 
         resultBox = (TextView) findViewById(R.id.resultsView);
 
+
         plusButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                double var1 = Double.parseDouble(varOneBox.getText().toString());
-                double var2 = Double.parseDouble(varTwoBox.getText().toString());
-                double sum = var1 + var2;
-                resultBox.setText(Double.toString(sum));
+            public void onClick(View view)
+            {
+                getVarTwo();
+                getVarOne();
+                result = var1+var2;
+                printResult();
+            }
+        });
+
+        minusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                getVarTwo();
+                getVarOne();
+                result = var1-var2;
+                printResult();
+            }
+        });
+
+        multiplyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                getVarTwo();
+                getVarOne();
+                result = var1*var2;
+                printResult();
+            }
+        });
+
+        divideButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                getVarTwo();
+                getVarOne();
+                if(var2!=0)
+                {
+                    result = var1 / var2;
+                    printResult();
+                }
+                else if (!fail2)
+                {
+                    Toast.makeText(MainActivity.this, "Cannot divide by 0", Toast.LENGTH_SHORT).show();
+                    resultBox.setText("Error");
+                }
+                else
+                {
+                    printResult();
+                }
             }
         });
     }
 
+    private void getVarTwo()
+    {
+        try
+        {
+            var2 = Double.parseDouble(varTwoBox.getText().toString());
+            fail2=false;
+        }
+        catch (Exception e)
+        {
+            fail2=true;
+            //Toast.makeText(MainActivity.this, "Invalid Input 2", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void getVarOne()
+    {
+        try
+        {
+            var1 = Double.parseDouble(varOneBox.getText().toString());
+            fail1=false;
+        }
+        catch (Exception e)
+        {
+            //Toast.makeText(MainActivity.this, "Invalid Input 2", Toast.LENGTH_SHORT).show();
+            fail1=true;
+        }
+    }
+
+    private void printResult()
+    {
+        if(!fail1&&!fail2)
+        {
+            resultBox.setText(Double.toString(result));
+        }
+        else if(fail1&&!fail2)
+        {
+            Toast.makeText(MainActivity.this, "Invalid Input 1", Toast.LENGTH_SHORT).show();
+            resultBox.setText("Error");
+        }
+        else if(!fail1&&fail2)
+        {
+            Toast.makeText(MainActivity.this, "Invalid Input 2", Toast.LENGTH_SHORT).show();
+            resultBox.setText("Error");
+        }
+        else
+        {
+            Toast.makeText(MainActivity.this, "Invalid Input 1 and Input 2", Toast.LENGTH_SHORT).show();
+            resultBox.setText("Error");
+        }
+    }
 }
